1fcbca449339486dfa6f165bccd463dc38a436b9
Luderius <dxgeva@gmail.com>
2022-01-22 15:56:40 -0500

    Added safe spaces

--- kill.js
+++ kill.js
@@ -2,6 +2,14 @@ function kill(pieces, piece) {
      let colorAbrInd = ["r", "b", "y", "g"];
      let colorFullInd = ["Red", "Blue", "Yellow", "Green"];
 
+     let safeSpaceX = [[0, 4], [4, 0], [10, 0], [0, 10], [14, 4], [4, 14], [14, 10], [10, 14]];
+
+     for (let x = 0; x < safeSpaceX.length; x++) {
+          if (compareArrs(piece.coords, safeSpaceX[x])) {
+               return;
+          }
+     }
+
      for (let x = 0; x < 4; x++) {
           if (piece.color !== colorFullInd[x]) {
                for (let i = 0; i < 4; i++) {
