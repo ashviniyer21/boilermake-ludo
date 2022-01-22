1c701be6c469c06048525ca3679534c55d921112
manasparanjape <mparanja@purdue.edu>
2022-01-22 11:32:45 -0500

    Complteed the move method

--- grid.js
+++ grid.js
@@ -1,21 +1,43 @@
 let pieces = [];
 let diceRoll = 0;
+
 class Piece {
-    constructor(index, color, location, button, clickable) {
+    constructor(index, color, button) {
         this.index = index;
         this.color = color;
-        this.location = location;
+        this.location = -1;
         this.button = button;
-        this.clickable = clickable
+        this.clickable = false;
         this.button.onclick = this.move(diceRoll);
+        this.coords = this.find_coordinates(index, color, location);
+    }
+
+    find_coordinates(index, color, location) {
+        let x = 0;
+        let y = 0;
+        if (location === -1) {
+            let colors = ["red", "blue", "yellow", "green"];
+            let colorDis = [[0, 0], [11, 0], [11, 11], [0, 11]];
+            let indexDis = [[0.5, 0.5], [2.5, 0.5], [2.5, 2.5], [0.5, 2.5]];
+
+            x = indexDis[index][0] + colorDis[colors.indexOf(color)][0];
+            y = indexDis[index][1] + colorDis[colors.indexOf(color)][1];
+        } else {
+
+        }
+
+
+        return [x, y]
     }
 
     updateButton() {
-
     }
 
     move(diceRoll) {
         if (this.location === -1) {
+            if (diceRoll !== 6) {
+                return false;
+            }
             this.location = 0;
         } else {
             this.location += diceRoll;
@@ -33,6 +55,7 @@ class Piece {
                 this.location = -2;
             }
         }
+        return true;
     }
 }
 
